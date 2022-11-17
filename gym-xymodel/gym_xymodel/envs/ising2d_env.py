edf8a3bd61daad0783ca2722399399aaab977d26
Michał Łukomski <michal.lukomski21@gmail.com>
2022-11-17 16:25:15 +0100

    should work?

--- gym-xymodel/gym_xymodel/envs/ising2d_env.py
+++ gym-xymodel/gym_xymodel/envs/ising2d_env.py
@@ -11,53 +11,46 @@ class Ising2DEnv(gym.Env):
 
     metadata = {"render.modes": ["human"]}
 
-    def __init__(self, side_len: int = 4):
+    def __init__(self, L: int = 4, J: float = 1.0):
         """Initialization of the gym environment"""
         # lattice side_len x side_len
-        self.side_len = side_len
+        self.L = L
+        self.J = J
 
-        self.observation_space = spaces.MultiBinary(self.side_len**2)
+        self.observation_space = spaces.MultiBinary(self.L**2)
         # states are -1 or 1
         self.state = self.observation_space.sample()
-        self.energy = self.compute_energy()
-        self.action_space = spaces.Discrete(self.side_len**2)
+        self.action_space = spaces.Discrete(self.L**2 + 1)
 
     def state_to_lattice(self):
         """
         Convert state to lattice [0,1] -> [-1,1]
         """
-        lattice = np.reshape(2 * self.state - 1, (self.side_len, self.side_len))
+        lattice = np.reshape(2 * self.state - 1, (self.L, self.L))
         return lattice
 
     def compute_energy(self):
         # J=0 except for nearest neighbor
         lattice = self.state_to_lattice()
-        energy = -sum(
+        energy = -self.J * sum(
             lattice[i, j] * (lattice[i - 1, j] + lattice[i, j - 1])
-            for i in range(self.side_len)
-            for j in range(self.side_len)
+            for i in range(self.L)
+            for j in range(self.L)
         )
         return energy
 
-    def finish_condition(self):
-        """
-        Finish condition is when energy is minimal, and all spins are the same
-        """
-        if np.all(self.state == self.state[0]):
-            return True
-        else:
-            return False
-
     def step(self, action):
-        self.state[action] = 1 if self.state[action] == 0 else 0
-        new_energy = self.compute_energy()
-        # finding minimal energy state, reward is difference between old and new energy
-        reward = -(new_energy - self.energy)
-        self.energy = new_energy
+        info = {}
 
-        # Done when energy is minimal, and all spins are the same
-        done = np.all(self.state == self.state[0])
-        info = {"energy": self.energy}
+        if action == self.L**2:
+            # finish action
+            reward = -self.compute_energy()
+            done = True
+        else:
+            self.state[action] = 1 if self.state[action] == 0 else 0
+            reward = 0
+            done = False
+
         return self.state, float(reward), bool(done), info
 
     def reset(self):
