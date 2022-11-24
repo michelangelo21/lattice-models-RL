8033c3390715d98681f26d7a2b7685b47c87b235
Michał Łukomski <michal.lukomski21@gmail.com>
2022-11-24 16:42:06 +0100

    compute energy -> np.roll

--- gym-xymodel/gym_xymodel/envs/xymodel_env.py
+++ gym-xymodel/gym_xymodel/envs/xymodel_env.py
@@ -11,33 +11,30 @@ class XYmodelEnv(gym.Env):
 
     metadata = {"render.modes": ["human"]}
 
-    def __init__(self, side_len: int = 4):
+    def __init__(self, L: int = 4, step_size: float = 0.1):
         # lattice side_len x side_len
-        self.side_len = side_len
+        self.L = L
+        self.step_size = step_size
 
         # self.observation_space = spaces.Box(
         #     low=0, high=255, shape=(1, side_len, side_len), dtype=np.uint8
         # ) # cnn
 
         self.observation_space = spaces.Box(
-            low=0, high=255, shape=(side_len**2,), dtype=np.uint8
+            low=0, high=1, shape=(L**2,), dtype=np.float32
         )  # mlp
 
         self.state = self.observation_space.sample()
-        self.energy = self.compute_energy()
 
-        # self.action_space = spaces.Box(
-        #     low=-5,
-        #     high=5,
-        #     shape=(1, self.side_len, self.side_len),
-        #     dtype=np.int8,
-        # ) # cnn not working
         self.action_space = spaces.Box(
-            low=-5,
-            high=5,
-            shape=(self.side_len**2,),
-            dtype=np.int8,
-        )  # mlp
+            low=-1,
+            high=1,
+            shape=(L**2,),
+            dtype=np.float32,
+        )
+
+        self.min_energy = float("inf")
+        self.min_state = None
 
         # self.action_space = spaces.Box(
         #     low=-1, high=1, shape=(self.side_len**2,), dtype=np.float32
@@ -45,11 +42,9 @@ class XYmodelEnv(gym.Env):
 
     def state_to_lattice(self):
         """
-        Convert state to lattice [-1,1] -> [-pi/2,pi/2]
+        Convert state to lattice [0, 1] -> [0, pi]
         """
-        lattice = np.reshape(
-            2 * np.pi / 256 * self.state, (self.side_len, self.side_len)
-        )
+        lattice = np.reshape(2 * np.pi * self.state, (self.L, self.L))
         return lattice
 
     def compute_energy(self):
@@ -58,27 +53,23 @@ class XYmodelEnv(gym.Env):
         """
         # J=0 except for nearest neighbor
         lattice = self.state_to_lattice()
-        energy = -sum(
-            np.cos(lattice[i, j] - lattice[i - 1, j])
-            + np.cos(lattice[i, i] - lattice[i, j - 1])
-            for i in range(self.side_len)
-            for j in range(self.side_len)
+        energy = (
+            -np.sum(
+                np.cos(lattice - np.roll(lattice, -1, axis=0))
+                + np.cos(lattice - np.roll(lattice, -1, axis=1))
+            )
+            / self.L**2
         )
+
+        if energy < self.min_energy:
+            self.min_energy = energy
+            self.min_state = self.state
         return energy
 
     def step(self, action):
-        # self.state = (self.state + 1 + action) % 2 - 1
-        # self.state[choosen_spin] = (
-        #     self.state[choosen_spin] + 1 + choosen_rotation
-        # ) % 2 - 1
-        self.state += np.uint8(action)
-        new_energy = self.compute_energy()
-        # finding minimal energy state, reward is difference between old and new energy
-        reward = -(new_energy - self.energy)
-        self.energy = new_energy
+        self.state = (self.state + self.step_size * action) % 1
 
-        done = bool(np.all(np.abs(self.state - self.state[0]) < 1e-6))
-        info = {"energy": self.energy}
+        info = {}
         return self.state, reward, done, info  # state, reward, done, info
 
     def reset(self):
